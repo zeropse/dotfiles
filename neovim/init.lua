98376a604f3c8bf409a9410f4db9d0fd3c25b165
Srijit Dey <129523087+zeropse@users.noreply.github.com>
2024-09-23 19:34:24 +0530

    Update to use mini.nvim

--- neovim/init.lua
+++ neovim/init.lua
@@ -28,4 +28,5 @@ require('lazy').setup {
   require 'plugins.indent-blankline',
   require 'plugins.misc',
   require 'plugins.comment',
+  require 'plugins.mini',
 }
---
+++ neovim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+      -- Here, you can configure any mini.nvim modules you want to use
+      -- For example, setting up 'mini.ai':
+      require('mini.ai').setup()
+  
+      -- You can configure other modules from mini.nvim here as well
+    end,
+  }
+  
